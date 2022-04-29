b4db0a3ae1603d5fec796c9f53ce8e6622ee09ea
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-04-29 16:55:25 +0700

    added urls to home hello world

--- LearnMVC/restaurants/views.py
+++ LearnMVC/restaurants/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    # return render(request, 'index.html')
+    return HttpResponse('Hello, World!')
