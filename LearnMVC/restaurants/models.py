055f396435364a202153ae9ba6cadef0cf67ce60
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-04-30 21:45:45 +0700

    change view on restaurant location

--- LearnMVC/restaurants/models.py
+++ LearnMVC/restaurants/models.py
@@ -9,3 +9,5 @@ class RestaurantLocation(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     my_date_field = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.name + ' - ' + self.location
