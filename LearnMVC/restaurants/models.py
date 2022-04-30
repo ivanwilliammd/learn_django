7abc812fa072652ad5dddf63ee523b9dcb7afae5
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-04-30 10:24:32 +0700

    manage.py makemigration, manage.py migrate

--- LearnMVC/restaurants/models.py
+++ LearnMVC/restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    category = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
