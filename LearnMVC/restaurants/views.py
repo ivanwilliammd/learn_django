77a913076718e842c88fd56b2d80b0d9ee914498
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-04-30 22:17:44 +0700

    listview adjustment

--- LearnMVC/restaurants/views.py
+++ LearnMVC/restaurants/views.py
@@ -23,21 +23,20 @@ class HomeView(TemplateView):
 
 
 # def restaurant_listview
-def restaurant_listview(request):
-    template_name = 'restaurants/restaurant_list.html'
-    queryset = RestaurantLocation.objects.all()
-    context = {
-        "restaurants" : queryset,
-        "title" : 'Restaurants | IvanMD',
-    }
-    return render(request, template_name, context)
+# def restaurant_listview(request):
+#     template_name = 'restaurants/restaurant_list.html'
+#     queryset = RestaurantLocation.objects.all()
+#     context = {
+#         "restaurants" : queryset,
+#         "title" : 'Restaurants | IvanMD',
+#     }
+#     return render(request, template_name, context)
 
 
 class RestaurantListView(ListView):
     queryset = RestaurantLocation.objects.all()
     
 class SearchRestaurantListView(ListView):
-    template_name = 'restaurants/restaurant_list.html'
     def get_queryset(self):
         print(self.kwargs)
         slug = self.kwargs.get('slug')
