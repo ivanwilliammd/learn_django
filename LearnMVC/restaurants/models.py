3929b6b44aa0bbf9065b3ec95798b7ebabefe104
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-05-15 14:21:03 +0700

    added slugify to utils, make migrations, and migrate

--- LearnMVC/restaurants/models.py
+++ LearnMVC/restaurants/models.py
@@ -1,6 +1,5 @@
 from unicodedata import category
 from django.db import models
-from django.utils.text import slugify
 
 
 # Create your models here.
@@ -11,26 +10,12 @@ class RestaurantLocation(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     my_date_field = models.DateField(auto_now=True)
+    slug = models.SlugField(null=True, blank=True)
+    
     def __str__(self):
         return self.name + ' - ' + self.location
 
+    @property
+    def title(self):
+        return self.name
 
-def unique_slug_generator(instance, new_slug=None):
-    """
-    This is for a Django project and it assumes your instance 
-    has a model with a slug field and a title character (char) field.
-    """
-    if new_slug is not None:
-        slug = new_slug
-    else:
-        slug = slugify(instance.title)
-
-    Klass = instance.__class__
-    qs_exists = Klass.objects.filter(slug=slug).exists()
-    if qs_exists:
-        new_slug = "{slug}-{randstr}".format(
-                    slug=slug,
-                    randstr=random_string_generator(size=4)
-                )
-        return unique_slug_generator(instance, new_slug=new_slug)
-    return slug
