0435c644dc835a4e301f0c4b40a42f5e5361f53f
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-05-15 14:27:53 +0700

    create signal on models to autocreate slugs

--- LearnMVC/restaurants/models.py
+++ LearnMVC/restaurants/models.py
@@ -1,6 +1,8 @@
 from unicodedata import category
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
+from .utils import unique_slug_generator
 
 # Create your models here.
 class RestaurantLocation(models.Model):
@@ -11,7 +13,7 @@ class RestaurantLocation(models.Model):
     updated = models.DateTimeField(auto_now=True)
     my_date_field = models.DateField(auto_now=True)
     slug = models.SlugField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name + ' - ' + self.location
 
@@ -19,3 +21,20 @@ class RestaurantLocation(models.Model):
     def title(self):
         return self.name
 
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    # print('saving..')
+    # print(instance.timestamp)
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+        instance.save()
+
+
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+#     print('saved')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
