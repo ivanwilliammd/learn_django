d06012cfb017ef5db380cdce2641c60f9f7db8dc
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-05-16 17:55:32 +0700

    added menus list and create

--- LearnMVC/menus/views.py
+++ LearnMVC/menus/views.py
@@ -18,7 +18,7 @@ class ItemDetailView(DetailView):
         return Item.objects.filter(user=self.request.user)
 
 class ItemCreateView(CreateView):
-
+    template_name = 'forms.html'
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
@@ -29,9 +29,13 @@ class ItemCreateView(CreateView):
         instance.user = self.request.user
         return super(ItemCreateView, self).form_valid(form)
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ItemCreateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Add Item'
+        return context
 
 class ItemUpdateView(UpdateView):
-
+    template_name = 'forms.html'
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
 
@@ -41,3 +45,8 @@ class ItemUpdateView(UpdateView):
         instance = form.save(commit=False)
         instance.user = self.request.user
         return super(ItemUpdateView, self).form_valid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ItemUpdateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Update Item'
+        return context
