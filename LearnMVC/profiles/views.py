2e48888e1f2283519334a99e1d9e88d2f901ab9a
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-05-19 21:51:00 +0700

    added restaurants get contacts on view

--- LearnMVC/profiles/views.py
+++ LearnMVC/profiles/views.py
@@ -1,3 +1,4 @@
+from cProfile import Profile
 from re import U
 from django.contrib.auth import get_user_model
 from django.http import Http404
@@ -5,6 +6,8 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
+from restaurants.models import RestaurantLocation
 # Create your views here.
 
 User = get_user_model()
@@ -18,3 +21,11 @@ class ProfileDetailView(DetailView):
         if username is None :
             raise Http404
         return get_object_or_404(User, username__iexact = username, is_active = True)
+
+    def get_context_data(self, *args, **kwargs) :
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        qs = RestaurantLocation.objects.filter(owner=self.get_object())
+        
+        if qs.exists():
+            context['locations'] = qs
+        return context
