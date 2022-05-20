f882e6fe98c194a0b97a1e4557430d0729211593
ivanwilliammd <ivanwilliam.md@gmail.com>
2022-05-20 17:13:37 +0700

    added profiles models and add migrations

--- LearnMVC/profiles/models.py
+++ LearnMVC/profiles/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save # ensure profile exists
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # user.profile
+    followers = models.ManyToManyField(User, related_name='followers', blank=True) # user.followers.all()
+    following = models.ManyToManyField(User, related_name='following', blank=True) # user.following.all()
+    
+    activated = models.BooleanField(default=False)
+    activation_key = models.CharField(max_length=120, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        profile, is_created = Profile.objects.get_or_create(user=instance)
+        # celery + redis
+        # send email
+
+post_save.connect(post_save_user_receiver, sender=User)
